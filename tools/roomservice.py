6c0d8b29688a9ad745b293806817628c0a686de3
Akhil Narang <akhilnarang@users.noreply.github.com>
2015-04-12 15:48:38 +0530

    Update roomservice.py
    Fix it
    BlackBox-OS >> BlackBox-devices

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -51,7 +51,7 @@ except:
     device = product
 
 if not depsonly:
-    print("Device %s not found. Attempting to retrieve device repository from BlackBox-OS Github (http://github.com/BlackBox-OS)." % device)
+    print("Device %s not found. Attempting to retrieve device repository from BlackBox-devices Github (http://github.com/BlackBox-devices)." % device)
 
 repositories = []
 
@@ -70,7 +70,7 @@ def add_auth(githubreq):
         githubreq.add_header("Authorization","Basic %s" % githubauth)
 
 if not depsonly:
-    githubreq = urllib.request.Request("https://api.github.com/search/repositories?q=%s+user:BlackBox-OS+in:name+fork:true" % device)
+    githubreq = urllib.request.Request("https://api.github.com/search/repositories?q=%s+user:BlackBox-devices+in:name+fork:true" % device)
     add_auth(githubreq)
     try:
         result = json.loads(urllib.request.urlopen(githubreq).read().decode())
@@ -173,12 +173,12 @@ def add_to_manifest(repositories, fallback_branch = None):
         repo_name = repository['repository']
         repo_target = repository['target_path']
         if exists_in_tree(lm, repo_name):
-            print('BlackBox-OS/%s already exists' % (repo_name))
+            print('BlackBox-devices/%s already exists' % (repo_name))
             continue
 
-        print('Adding dependency: BlackBox-OS/%s -> %s' % (repo_name, repo_target))
+        print('Adding dependency: BlackBox-devices/%s -> %s' % (repo_name, repo_target))
         project = ElementTree.Element("project", attrib = { "path": repo_target,
-            "remote": "github", "name": "BlackBox-OS/%s" % repo_name })
+            "remote": "github", "name": "BlackBox-devices/%s" % repo_name })
 
         if 'branch' in repository:
             project.set('revision',repository['branch'])
@@ -200,7 +200,7 @@ def add_to_manifest(repositories, fallback_branch = None):
 
 def fetch_dependencies(repo_path, fallback_branch = None):
     print('Looking for dependencies')
-    dependencies_path = repo_path + '/BlackBox.dependencies'
+    dependencies_path = repo_path + '/blackbox.dependencies'
     syncable_repos = []
 
     if os.path.exists(dependencies_path):
@@ -209,7 +209,7 @@ def fetch_dependencies(repo_path, fallback_branch = None):
         fetch_list = []
 
         for dependency in dependencies:
-            if not is_in_manifest("BlackBox-OS/%s" % dependency['repository']):
+            if not is_in_manifest("BlackBox-devices/%s" % dependency['repository']):
                 fetch_list.append(dependency)
                 syncable_repos.append(dependency['target_path'])
 
@@ -292,4 +292,4 @@ else:
             print("Done")
             sys.exit()
 
-print("Repository for %s not found in the BlackBox-OS Github repository list. If this is in error, you may need to manually add it to your local_manifests/roomservice.xml." % device)
+print("Repository for %s not found in the BlackBox-devices Github repository list. If this is in error, you may need to manually add it to your local_manifests/roomservice.xml." % device)
